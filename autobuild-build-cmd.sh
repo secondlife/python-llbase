e6f80562d2e8247b8abdd6f212074de8c9029e57
Oz Linden <oz@lindenlab.com>
2017-06-08 12:12:18 -0400

    revamp autobuild packaging

---
+++ autobuild-build-cmd.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -x
+
+# copy the license file to where autobuild wants it (required by autobuild)
+test -d LICENSES || mkdir LICENSES
+cp ../docs/llbase-license.txt LICENSES
+
+#### TBD - find a way to run the self tests as part of all this
+
+# create a source distribution of the llbase package
+(cd ..; python setup.py sdist --format gztar)
+
+# extract the version number from the distribution tarball name
+dist_tarball=../dist/llbase-*.tar.gz
+version=$(echo $dist_tarball | sed -E 's/.*llbase-//; s/\.tar.gz//;')
+# create the version_file required by autobuild, adding the codeticket number
+echo "$version.${AUTOBUILD_BUILD_ID:-0}" > VERSION.txt
+
+# install the source package in a virtual environment
+virtualenv venv
+. venv/bin/activate
+pip install $dist_tarball
+
+# find the path where the installed package files are in the virtual environment
+installed_basedir=$(dirname $(python -c 'import llbase; print llbase.__file__;'))
+
+# create the distribution tree we want for python as installed by autobuild
+mkdir -p lib/python
+# copy the installed files from the virtual environment to the autobuild conventional tree
+cp -r "${installed_basedir}" lib/python
+# clean out the self test files from the autobuild conventional tree
+rm -rf lib/python/llbase/test
