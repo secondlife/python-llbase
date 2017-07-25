693de1b90f61be571e1671b45b94f9f1aec583d8
Tapple Gao <tapplek@gmail.com>
2017-07-24 19:12:55 -0700

    full version compatability declaration

--- setup.py
+++ setup.py
@@ -40,8 +40,10 @@ CLASSIFIERS = """\
 Development Status :: 5 - Production/Stable
 Intended Audience :: Developers
 License :: OSI Approved :: MIT License
-Programming Language :: Python
-Programming Language :: Python :: 3
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Programming Language :: C
 Topic :: Software Development
 Topic :: Software Development :: Libraries :: Python Modules
