2c5fd70a1ade24eda7afb541cad61535c00d908a
Nat Goodspeed <nat@lindenlab.com>
2018-02-22 11:05:39 -0500

    MAINT-8310: Use cgitb to format exception traceback for json logging.
    Align the JsonFormatter subclass of logging.Formatter more closely with
    documented Formatter protocol:
    - call formatTime() and formatException() from within format()
    - support formatTime(record, datefmt=None)
      - if datefmt is passed, use strftime() and also the 'converter' attribute
    - support formatException(exc_info)
      - use cgitb to format the passed exc_info
    
    Add a few unit tests for lljsonlog.

--- llbase/lljsonlog.py
+++ llbase/lljsonlog.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import
 
+import cgitb
 from datetime import datetime
 import json
 import logging
+from StringIO import StringIO
+import time
 import traceback
 
 # This module provides a Python logging formatter that serializes messages into a JSON object suitable for logging to BNW MMA.
@@ -39,29 +42,62 @@ import traceback
 # The uninteresting keys were discovered by dumping what's in a typical LogRecord's __dict__.
 # Ideally we'd have a better way to figure these out, since technically then
 # they could change with Python version. But eh.
-UNINTERESTING_LOGRECORD_KEYS = {'args', 'created', 'exc_info', 'exc_text', 'filename', 'funcName', 'levelname', 'levelno', 'lineno', 'module', 'msecs', 'msg', 'name', 'pathname', 'process', 'processName', 'relativeCreated', 'thread', 'threadName'}
+UNINTERESTING_LOGRECORD_KEYS = {
+    'args', 'created', 'exc_info', 'exc_text', 'filename', 'funcName', 'levelname', 'levelno',
+    'lineno', 'module', 'msecs', 'msg', 'name', 'pathname', 'process', 'processName',
+    'relativeCreated', 'thread', 'threadName',
+}
 """Uninteresting data found in a `logging.LogRecord` instance."""
 
 class JsonFormatter(logging.Formatter):
-
     """A `logging.Formatter` that formats records as JSON objects."""
+
+    def __init__(self):
+        # Format times in GMT
+        # https://docs.python.org/2/library/logging.html#formatter-objects
+        self.converter = time.gmtime
 
     def format(self, record):
         data = {
             'name': record.name,  # logger name
             'level': record.levelname,
             'msg': record.getMessage(),
-            'time': datetime.fromtimestamp(record.created).isoformat() + 'Z',
+            'time': self.formatTime(record),
         }
         if record.exc_info:
             exc_type, exc, exc_trace = record.exc_info
             data.update({
                 'error_type': exc_type.__name__,
                 'error_message': unicode(exc),
-                'traceback': '\n'.join(traceback.format_tb(exc_trace, 10)),
+                'traceback': self.formatException(record.exc_info),
             })
 
         # Include any 'extra' data.
-        data.update({k: v for k, v in record.__dict__.items() if k not in UNINTERESTING_LOGRECORD_KEYS})
+        data.update({k: v for k, v in record.__dict__.items()
+                     if k not in UNINTERESTING_LOGRECORD_KEYS})
 
         return json.dumps(data, default=repr)
+
+    def formatTime(self, record, datefmt=None):
+        if datefmt:
+            # If caller explicitly passed a time.strftime() format, use it as
+            # directed. Convert to gmtime() or localtime() according to our
+            # converter attribute.
+            return time.strftime(datefmt, self.converter(record.created))
+        else:
+            # If caller did not pass datefmt, use datetime.isoformat() to
+            # include microseconds, which time.strftime() cannot support
+            # because time.struct_time() has no field for fractional seconds.
+            return datetime.fromtimestamp(record.created).isoformat() + 'Z'
+
+    def formatException(self, exc_info):
+        # cgitb.Hook writes its output to the file-like object provided to its
+        # constructor. As we don't want to accumulate successive formatted
+        # exceptions, we need a new StringIO instance every time. (There is no
+        # method to clear a StringIO instance.) But as there is also no method
+        # to replace the stream used by a Hook instance, we must therefore
+        # instantiate a new Hook every time.
+        stream = StringIO()
+        hook = cgitb.Hook(file=stream, format="text")
+        hook.handle(exc_info)
+        return stream.getvalue()
